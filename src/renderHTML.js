3d5d7d4f557c472631267321641bc8526df9b7ed
JonLedo <jonledo.code@gmail.com>
2022-11-04 21:13:54 -0400

    updated the renderHTML function and it now correctly takes input data, and creates an HTML file using the inputs with the help of three helper functions (parseManagerData, parse....)

--- src/renderHTML.js
+++ src/renderHTML.js
@@ -1,4 +1,81 @@
+// Because the data incoming from the prompts are arrays likely holding many objects, these helper functions are needed
+function parseManagerData(arrayData) {
+  const managers = arrayData
+    .map((data) => {
+      return `
+      <!-- START OF CARD -->
+      <div class="card col-md-4 col-12">
+        <header class="card-header">
+          <h2>${data.managerName}</h2>
+          <p>Manager</p>
+        </header>
+        <div class="card-body">
+          <p class="card-text">ID: ${data.managerID}</p>
+          <p class="card-text">Email: <a href="${data.managerEmail}">${data.managerName}'s Email</a></p>
+          <p class="card-text">Office Number: ${data.office}</p>
+        </div>
+      </div>
+      <!-- END OF A CARD -->
+    `
+    })
+    .join('')
+
+  return managers
+}
+
+function parseEngineerData(arrayData) {
+  const engineers = arrayData
+    .map((data) => {
+      return `
+    <!-- START OF CARD -->
+    <div class="card col-md-4 col-12">
+      <header class="card-header">
+        <h2>${data.engineerName}</h2>
+        <p>Engineer</p>
+      </header>
+      <div class="card-body">
+        <p class="card-text">ID: ${data.engineerID}</p>
+        <p class="card-text">Email: <a href="${data.engineerEmail}">${data.engineerName}'s Email</a></p>
+        <p class="card-text">Github: ${data.github}</p>
+      </div>
+    </div>
+    <!-- END OF A CARD --> 
+    `
+    })
+    .join('')
+
+  return engineers
+}
+
+function parseInternData(arrayData) {
+  const interns = arrayData
+    .map((data) => {
+      return `
+    <!-- START OF CARD -->
+    <div class="card col-md-4 col-12">
+      <header class="card-header">
+        <h2>${data.internName}</h2>
+        <p>Intern</p>
+      </header>
+      <div class="card-body">
+        <p class="card-text">ID: ${data.internID}</p>
+        <p class="card-text">Email: <a href="${data.internEmail}">${data.internName}'s Email</a></p>
+        <p class="card-text">School: ${data.school}</p>
+      </div>
+    </div>
+    <!-- END OF A CARD -->
+    `
+    })
+    .join('')
+
+  return interns
+}
+
 function renderHTML(managerData, engineerData, internData) {
+  const manager = parseManagerData(managerData)
+  const engineer = parseEngineerData(engineerData)
+  const intern = parseInternData(internData)
+
   return `
   <!DOCTYPE html>
   <html lang="en">
@@ -15,70 +92,20 @@ function renderHTML(managerData, engineerData, internData) {
       />
       <link rel="stylesheet" href="" />
       <script src="" defer></script>
-      <title>Team Manager</title>
+      <title>Team Management System</title>
     </head>
     <body>
       <header class="row bg-primary mb-5">
-        <h1 class="text-center m-3">My Team</h1>
+        <h1 class="text-center m-3">${managerData[0].managerName}'s Team</h1>
       </header>
 
       <div class="container">
         <main class="row">
-          <!-- START OF CARD -->
-          <div class="card col-md-4 col-12">
-            <header class="card-header">
-              <h2>Manager Name</h2>
-              <p>Manager</p>
-            </header>
-            <div class="card-body">
-              <p class="card-text">ID: 1</p>
-              <p class="card-text">Email: <a href="#">email link</a></p>
-              <p class="card-text">Office Number: 1</p>
-            </div>
-          </div>
-          <!-- END OF A CARD -->
+          ${manager}
 
-          <!-- START OF CARD -->
-          <div class="card col-md-4 col-12">
-            <header class="card-header">
-              <h2>Engineer Name</h2>
-              <p>Engineer</p>
-            </header>
-            <div class="card-body">
-              <p class="card-text">ID: 2</p>
-              <p class="card-text">Email: <a href="#">email link</a></p>
-              <p class="card-text">Github: gitlink</p>
-            </div>
-          </div>
-          <!-- END OF A CARD -->
+          ${engineer}
 
-          <!-- START OF CARD -->
-          <div class="card col-md-4 col-12">
-            <header class="card-header">
-              <h2>Engineer Name</h2>
-              <p>Engineer</p>
-            </header>
-            <div class="card-body">
-              <p class="card-text">ID: 3</p>
-              <p class="card-text">Email: <a href="#">email link</a></p>
-              <p class="card-text">Github: gitlink</p>
-            </div>
-          </div>
-          <!-- END OF A CARD -->
-
-          <!-- START OF CARD -->
-          <div class="card col-md-4 col-12">
-            <header class="card-header">
-              <h2>Intern Name</h2>
-              <p>Intern</p>
-            </header>
-            <div class="card-body">
-              <p class="card-text">ID: 4</p>
-              <p class="card-text">Email: <a href="#">email link</a></p>
-              <p class="card-text">School: Bootcamp</p>
-            </div>
-          </div>
-          <!-- END OF A CARD -->
+          ${intern}
         </main>
       </div>
     </body>

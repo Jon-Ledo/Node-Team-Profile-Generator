d205953299667bc409823e987f0fa7ceb04c1eb9
JonLedo <jonledo.code@gmail.com>
2022-11-07 01:46:03 -0500

    added a new function to add a choices menu inbetween user prompts. Menu appears after each round of questions, letting user choose between an engineer, intern or option to finish building the team. A new menuQuestions file made in src folder.

--- src/managerQuestions.js
+++ src/managerQuestions.js
@@ -28,3 +28,13 @@ const managerQuestions = [
 ]
 
 module.exports = managerQuestions
+
+
+// menu
+// add an engineer
+// add an intern
+// finish building my team 
+
+// select engineer
+// fill inputs
+// go back to previous menu
